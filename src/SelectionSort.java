3301651b8208ad87a1258bd09461ccd88dbb2c3c
Yoonsung <lvev9925@naver.com>
2015-04-30 22:17:34 +0900

    (Selection Sort) add algorithm

---
+++ src/SelectionSort.java
@@ -0,0 +1,22 @@
+/**
+ * Created by yoon on 15. 4. 30..
+ */
+public class SelectionSort implements Sort {
+    @Override
+    public int[] sort(int[] arr) {
+
+        int size = arr.length;
+        int maxValueIndex = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size-i; j++) {
+                if (arr[j] > arr[maxValueIndex])
+                    maxValueIndex = j;
+            }
+            Util.swap(arr, size-i-1, maxValueIndex);
+            maxValueIndex = 0;
+        }
+
+        return arr;
+    }
+}
